e4961b65d18f2c681e9e74b0fd7c0852eb1a80dc
Nobu <nowobuwu@gmail.com>
2021-11-04 20:17:14 -0500

    fix parsing issues

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,5 +2,4 @@ declare module "dotlang" {
     export function parse(path: string): Map<string, string>
     export function parseMultiple(paths: string[]): Map<string, Map<string, string>>
     export function parseAllInDir(dir: string): Map<string, Map<string, string>>
-    export function parseString(str: string): String
 }
