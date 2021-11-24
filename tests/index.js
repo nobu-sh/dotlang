8f8ac3f61071eca454bae98020c852941c936c42
Nobu <nowobuwu@gmail.com>
2021-11-24 08:32:57 -0600

    update tests accordingly

--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,13 @@
 const { parse, parseMultiple, parseAllInDir } = require('../lib/main')
 
+console.log("--- Single Parse ---")
 const map = parse('tests/lang/en_US.lang')
 console.log(map)
 
+console.log("--- Multiple Parse ---")
 const multiple = parseMultiple([ 'tests/lang/en_US.lang', 'tests/lang/en_ES.lang' ])
 console.log(multiple)
 
+console.log("--- All Parse ---")
 const dir = parseAllInDir('tests/')
 console.log(dir)
